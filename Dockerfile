a603a905f01951eaa0fd26ed84e0ff4f8d45d1fc
gildarivera <gilda.rivera@telus.com>
2022-04-13 16:36:10 -0600

    feat: api and files

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node
+WORKDIR /userGKE
+COPY package*.json ./
+RUN npm install
+COPY . .
+ENV DB_HOST=localhost
+ENV DB_USER=root
+ENV DB_PASSWORD=gilda
+ENV DB_NAME=user
+ENV DB_PORT=3306
+#ENV DB_PORT=3000
+ENV NODE_DOCKER_PORT=8080  
+EXPOSE 8080
+CMD ["npm","start"]
+
+
+
--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "gilda",
-    DB: "user"
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
+    PORT:process.env.DB_PORT
   };
